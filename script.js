984766fdd23492e16e6a59cf1aa8142e7b963718
liyababu <50897099+liyababu@users.noreply.github.com>
2019-12-09 18:39:13 -0600

    added code

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.getElementById("launchForm");
+   form.addEventListener("submit", function(event) {
+      let pilotnameInput = document.querySelector("input[name=pilotName]");
+      let copilotnameInput = document.querySelector("input[name=copilotName]");
+      let fuellevelInput = document.querySelector("input[name=fuelLevel]");
+      if (pilotnameInput.value === "") {
+         alert("Pilot name required!");
+         // stop the form submission
+         event.preventDefault();
+      }
+      if (copilotnameInput.value === "") {
+         alert("CoPilot name required!");
+         // stop the form submission
+         event.preventDefault();
+      }
+      if (typeof fuellevelInput.value === "" ){  
+         alert("Enter Fuel level");  
+         event.preventDefault();
+           
+       }else if(typeof fuellevelInput.value !== 'number'){  
+         alert("Enter Numeric value only")  
+         event.preventDefault();
+         }  
+       
+         
+      
+      
+   });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
